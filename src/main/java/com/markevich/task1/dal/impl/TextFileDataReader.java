0799c8daa566a9a4cc29a46f0b06d9f1591ad539
HeatLee <unheatlee@gmail.com>
2020-02-07 19:47:55 +0300

    Include factory to data reader

--- src/main/java/com/markevich/task1/dal/impl/TextFileDataReader.java
+++ src/main/java/com/markevich/task1/dal/impl/TextFileDataReader.java
@@ -4,6 +4,8 @@ import com.markevich.task1.dal.DataReader;
 import com.markevich.task1.dal.util.Converter;
 import com.markevich.task1.entity.Employee;
 import com.markevich.task1.exception.DataAccessException;
+import com.markevich.task1.factory.FactoryImpl;
+import com.sun.org.apache.bcel.internal.generic.FADD;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -13,12 +15,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TextFileDataReader implements DataReader {
+    private static final String DEVELOPER_EMPLOYEE = "Developer";
+    private static final String QA_ENGINEER_EMPLOYEE = "QAEngineer";
+    private static final int EMPLOYEE_TYPE_INFO = 0;
+    private static final String REGEX = "\\|";
+
     private String filePath;
 
     public TextFileDataReader(String filePath) {
         this.filePath = filePath;
     }
-
 
     @Override
     public List<Employee> readAll() {
@@ -46,7 +52,18 @@ public class TextFileDataReader implements DataReader {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                Employee e = Converter.parseToObject(line);
+                String[] fields = line.split(REGEX);
+                Employee e;
+                switch (fields[EMPLOYEE_TYPE_INFO]) {
+                    case DEVELOPER_EMPLOYEE:
+                        e = FactoryImpl.getInstance().createDeveloper(fields);
+                        break;
+                    case QA_ENGINEER_EMPLOYEE:
+                        e = FactoryImpl.getInstance().createQAEngineer(fields);
+                        break;
+                    default:
+                        e = null;
+                }
                 if (e != null) {
                     list.add(e);
                 }
