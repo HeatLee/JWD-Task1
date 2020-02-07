becfce3e7c3b7e2410905fc8a4541e0e7b2d01e0
HeatLee <unheatlee@gmail.com>
2020-02-07 19:45:03 +0300

    Fix validator

--- src/main/java/com/markevich/task1/validator/DeveloperValidator.java
+++ src/main/java/com/markevich/task1/validator/DeveloperValidator.java
@@ -1,6 +1,6 @@
 package com.markevich.task1.validator;
 
-import com.markevich.task1.entity.Developer;
+import com.markevich.task1.entity.developer.Developer;
 
 public class DeveloperValidator {
     public static boolean validate(Developer developer) {
--- src/main/java/com/markevich/task1/validator/QAEngineerValidator.java
+++ src/main/java/com/markevich/task1/validator/QAEngineerValidator.java
@@ -1,6 +1,6 @@
 package com.markevich.task1.validator;
 
-import com.markevich.task1.entity.QAEngineer;
+import com.markevich.task1.entity.qa.QAEngineer;
 
 public class QAEngineerValidator {
     public static boolean validate(QAEngineer engineer) {
