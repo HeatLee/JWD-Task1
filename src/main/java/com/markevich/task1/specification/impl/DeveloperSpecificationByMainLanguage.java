67d8483e29bb035022782f2aa972cd3de975d309
HeatLee <unheatlee@gmail.com>
2020-02-07 19:50:16 +0300

    Fix specification impl

--- src/main/java/com/markevich/task1/specification/impl/DeveloperSpecificationByMainLanguage.java
+++ src/main/java/com/markevich/task1/specification/impl/DeveloperSpecificationByMainLanguage.java
@@ -1,8 +1,8 @@
 package com.markevich.task1.specification.impl;
 
-import com.markevich.task1.entity.Developer;
+import com.markevich.task1.entity.developer.Developer;
 import com.markevich.task1.entity.Employee;
-import com.markevich.task1.entity.ProgrammingLanguage;
+import com.markevich.task1.entity.developer.ProgrammingLanguage;
 import com.markevich.task1.specification.EmployeeSpecification;
 
 public class DeveloperSpecificationByMainLanguage implements EmployeeSpecification {
--- src/main/java/com/markevich/task1/specification/impl/QAEngineerSpecificationByQATool.java
+++ src/main/java/com/markevich/task1/specification/impl/QAEngineerSpecificationByQATool.java
@@ -1,8 +1,8 @@
 package com.markevich.task1.specification.impl;
 
 import com.markevich.task1.entity.Employee;
-import com.markevich.task1.entity.QAEngineer;
-import com.markevich.task1.entity.QATools;
+import com.markevich.task1.entity.qa.QAEngineer;
+import com.markevich.task1.entity.qa.QATools;
 import com.markevich.task1.specification.EmployeeSpecification;
 
 public class QAEngineerSpecificationByQATool implements EmployeeSpecification {
